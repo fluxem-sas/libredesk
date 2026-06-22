1302d76214101dd0facf6b1cf02d36270deaa685
luislondo03 <luis.martinez@fluxem.co>
2026-06-21 21:09:55 -0500

    feat: add meta and custom attributes fields to user queries

--- internal/user/queries.sql
+++ internal/user/queries.sql
@@ -41,6 +41,8 @@ SELECT
     u.phone_number_country_code,
     u.phone_number,
     u.country,
+    u.meta,
+    u.custom_attributes,
     u.api_key,
     u.api_key_last_used_at,
     u.external_user_id,
@@ -293,6 +295,8 @@ SELECT
     u.phone_number_country_code,
     u.phone_number,
     u.country,
+    u.meta,
+    u.custom_attributes,
     u.api_key,
     u.api_key_last_used_at,
     u.api_secret,
@@ -420,3 +424,4 @@ SELECT
 
 -- name: get-user-ids-by-role
 SELECT user_id FROM user_roles WHERE role_id = $1;
+
